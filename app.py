6193b91467dc35783fc371a91c630f40abc7796c
yishate <irsyadjamasuri@gmail.com>
2026-04-05 19:28:28 +0800

    app.py

--- app.py
+++ app.py
@@ -48,7 +48,10 @@ with st.sidebar:
     st.title("Data Input")
     st.markdown("Enter gas concentrations (ppm)")
     
-    oil_type = st.radio("Select Insulating Fluid:", ["Mineral Oil", "Natural Ester (NE)"])
+    oil_type = st.radio(
+        "Select Insulating Fluid:", 
+        ["Mineral Oil", "Natural Ester (NE)"]
+    )
     st.divider()
     
     h2 = st.number_input("Hydrogen (H2)", min_value=0.0, value=10.0, step=1.0)
@@ -82,9 +85,7 @@ with tab1:
     with col_chart:
         if (ch4 + c2h4 + c2h2) > 0:
             fig = go.Figure()
+            
             # Triangle Zones
-            fig.add_trace(go.Scatterternary(a=[98, 100, 98], b=[0, 0, 2], c=[2, 0, 0], mode='lines', fill='toself', fillcolor='rgba(128, 0, 0, 0.6)', line=dict(color='black', width=1), name='PD'))
-            fig.add_trace(go.Scatterternary(a=[98, 98, 76, 80], b=[0, 2, 4, 0], c=[2, 0, 20, 20], mode='lines', fill='toself', fillcolor='rgba(255, 165, 0, 0.6)', line=dict(color='black', width=1), name='T1'))
-            fig.add_trace(go.Scatterternary(a=[80, 76, 46, 50], b=[0, 4, 4, 0], c=[20, 20, 50, 50], mode='lines', fill='toself', fillcolor='rgba(255, 200, 150, 0.8)', line=dict(color='black', width=1), name='T2'))
-            fig.add_trace(go.Scatterternary(a=[50, 46, 31, 0, 0], b=[0, 4, 15, 15, 0], c=[50, 50, 54, 85, 100], mode='lines', fill='toself', fillcolor='rgba(139, 69, 19, 0.6)', line=dict(color='black', width=1), name='T3'))
-            fig.
+            fig.add_trace(go.Scatterternary(
+                a=[98, 100, 98], b=[0, 0, 2], c=[2, 0,
